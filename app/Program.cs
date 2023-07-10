c1325c46373b39ef7157c8192258a70391f0af63
ulrichmain <ulrichmain28@gmail.com>
2023-07-10 15:03:51 +0200

    base files implemented

--- app/Program.cs
+++ app/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿class Client{
+    long id;
+    string pictureString;
+    string firstName;
+    string secondName;
+    int gender;
+    string race;
+    string University;
+    string Degree;
+    string[] comments;
+    string JobTitle;
+
+    
+}
