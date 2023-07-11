b1d6199227af28dbc57e5523d599f42743eb046f
ulrichmain <ulrichmain28@gmail.com>
2023-07-11 10:36:13 +0200

    added checking functions

--- app/Candidate.cs
+++ app/Candidate.cs
@@ -1,17 +1,18 @@
 ﻿namespace excelParser{   
 
 
-class Candidate{
-    long id;
-    string pictureString;
-    string name;
-    string surname;
-    int gender;
-    string race;
-    string university;
-    string degree;
-    string comments;
-    string jobTitle;
+public class Candidate{ //do not think it has to be private?
+    public string? number;
+    public string? pictureString;
+    public string? name;
+    public string? surname;
+    public string? location;
+    public string? gender;
+    public string? race;
+    public string? university;
+    public string? degree;
+    public string? comments;
+    public string? jobTitle;
 }
 
 }
--- app/excelParser.cs
+++ app/excelParser.cs
@@ -1,4 +1,6 @@
 
+using System.Reflection;
+
 namespace excelParser{
 
     class Parser
@@ -6,11 +8,83 @@ namespace excelParser{
         Candidate[] candidates;
 
 
-
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            Candidate candidate = new Candidate();
+            candidate.number = "1";
+            candidate.pictureString = "1232";
+            candidate.name = "Jessica";
+            candidate.surname = "Parker";
+            candidate.location = "Sandton";
+            candidate.gender = "Other";
+            candidate.race = "White";
+            candidate.university = "University of Johannesburg";
+            candidate.degree = "Bsc in Computer Science";
+            candidate.comments = "hard working mfer";
+            candidate.jobTitle = "Software Engineer";
+
+            noNullValues(candidate);
         }
+
+
+        static Boolean isValidCandiate(Candidate candidate)
+        {
+            return true;
+        }
+
+        static Boolean noNullValues(Candidate candidate)
+        {
+        }
+
+        static Boolean isValidNumber(string number)
+        {
+            return true;
+        }
+
+        static Boolean isValidPicture(string picture)
+        {
+            return true;
+        }
+
+        static Boolean isValidName(string name)
+        {
+            return true;
+        }
+
+        static Boolean isValidLocation(string location)
+        {
+            return true;
+        }
+
+        static Boolean isValidGender(string gender)
+        {
+            return true;
+        }
+
+
+        static Boolean isValidUniversity(string university)
+        {
+            return true;
+        }
+
+        static Boolean isValidDegree(string degree)
+        {
+            return true;
+        }
+
+
+        static Boolean isValidComment(string comment)
+        {
+            return true;
+        }
+
+        static Boolean isValidJobTitle(string job)
+        {
+            return true;
+        }
+
+
+
     }
 
 
