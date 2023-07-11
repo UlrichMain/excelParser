51d92b61543cf38247b261a2453c7bf668447f01
RICHARD MILNE <220036931@student.uj.ac.za>
2023-07-11 08:55:42 +0200

    Added Candidate data gathering

--- app/Candidate.cs
+++ app/Candidate.cs
@@ -10,6 +10,25 @@ class Client{
     string degree;
     string comments;
     string jobTitle;
+
+        public Client(long tID, string cPic, string fName, string sName, int cGen, string cRace, string uni, string cDeg, string com, string cJob)
+        {
+            id = tID;
+            pictureString = cPic;
+            name = fName;
+            surname = sName;
+            gender = cGen;
+            race = cRace;
+            university = uni;
+            degree = deg;
+            comments = com;
+            jobTitle = cJob;
+        }
+
+        public toString()
+        {
+            Console.print("TestData");
+        }
 }
 
 }
--- app/excelParser.cs
+++ app/excelParser.cs
@@ -1,10 +1,40 @@
 using Candidate;
 
-namespace excelParser{
+namespace excelParser {
 
     Candidate[] candidates;
     Candidate temp = new Candidate();
-    
+
+    public void getClients()
+    {
+        string path = "C:\Users\USER\Downloads\ExcelParser.csv";
+        StreamReader strRead = null;
+        if (File.exists(path))
+        {
+            strRead = new SreamReader(File.openRead(path));
+            List<string> clientVals = new List<string>();
+            var headers = strRead.ReadLine();
+            while (!strRead.EndOfStream)
+            {
+                var cLine = strRead.ReadLine();
+                var cVals = cLine.Split(',');
+                long cID = cVals[0];
+                string pictureString = cVals[1];
+                string name = cVals[2];
+                string surname = cVals[3];
+                int gender = cVals[4];
+                string race = cVals[5];
+                string university = cVals[6];
+                string degree = cVals[7];
+                string comments = cVals[8];
+                string jobTitle = cVals[9];
+            }
+
+        }
+
+
+
+    }
 
 
 }
