750e34544cff85315b3f15265cce49e74761a83c
RICHARD MILNE <220036931@student.uj.ac.za>
2023-07-13 09:20:42 +0200

    Merging new classes into main branch

---
+++ app/Journey.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+public class Journey
+{
+	private Status stat;
+	private Gender gen;
+	private OpenDay OD;
+	private string comments;
+	private DateTime date;
+
+	public Journey(Status stat, Gender gen, OpenDay OD, string comments, DateTime date)
+	{
+		this.stat = stat;
+		this.gen = gen;
+		this.OD = OD;
+		this.comments = comments;
+		this.date = date;
+	}
+
+	public void toString()
+	{
+		Console.WriteLine(string.Format(" Candidate Journey:\nStatus: {0} |Gender: {1} |OpenDay: {2} |Date: {3} " +
+			"\nComments:{4}", this.stat, this.gen, this.OD, this.date, this.comments));
+	}
+
+	public Status getStat()
+	{
+		return stat;
+	}
+
+	public Gender getGen()
+	{
+		return gen;
+	}
+
+	public OpenDay getOD()
+	{
+		return OD;
+	}
+
+	public string getCom()
+	{
+		return comments;
+	}
+
+	public DateTime getDate()
+	{
+		return date;
+	}
+
+    public void setStat(Status up)
+    {
+        this.stat = up;
+    }
+
+    public void setGen(Gender up)
+    {
+        this.gen = up;
+    }
+
+    public void setOD(OpenDay up)
+    {
+        this.OD = up;
+    }
+
+    public void setCom(string up)
+    {
+        this.comments = up;
+    }
+
+    public void setDate(DateTime up)
+    {
+        this.date = up;
+    }
+}
---
+++ app/OpenDay.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public enum OpenDay
+{
+	Scheduled, 
+	Attended,
+	Reshceduled
+}
